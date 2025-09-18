a5608495bb1b82c81f371a71a4f2ac6633994e9d
stytv <stytv14@gmail.com>
2025-09-18 11:10:56 +0530

    Delete v0-e-commerce-ui-design - Copy directory

--- v0-e-commerce-ui-design - Copy/lib/supabase.js
+++
@@ -1,8 +0,0 @@
-// lib/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-// Use environment variables for security
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
